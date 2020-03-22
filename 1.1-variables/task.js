863945b7f5aa02e8f0aaed4eb9f6ce8064fb9011
Olga Rosokha <helga2004@list.ru>
2020-03-22 20:13:20 +0300

    Second commit

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -4,16 +4,13 @@ function averageMark(a, g, p) {
 	let algebra = a;
 	let geography = g;
 	let physics = p;
-	let result = (algebra + geography + physics) / 3
-    
+	let result = (algebra + geography + physics) / 3;
 	return result;
 }
 
 function sayHello(userName) {
 	let myName = userName;
 	let message = 'Привет, мир! Меня зовут ' + myName +'.';
-    
-    
 	return message;
 }
 
@@ -22,6 +19,5 @@ function calculateFormula() {
     let y = 22;
     let z = 0;
     let result = x * y + 5 * z + x - 1; 
-	
     return result;
 }
