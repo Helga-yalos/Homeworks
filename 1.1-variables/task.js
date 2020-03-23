7e01f129ca7ca188141e7df263a8ea338b710de0
Olga Rosokha <helga2004@list.ru>
2020-03-23 10:09:05 +0300

    Задача 2 доработана

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -10,7 +10,7 @@ function averageMark(a, g, p) {
 
 function sayHello(userName) {
 	let myName = userName;
-	let message = 'Привет, мир! Меня зовут ' + myName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
 	return message;
 }
 
