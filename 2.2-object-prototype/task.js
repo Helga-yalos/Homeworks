2802c46687301cb201971db072f2627e5975ac0b
Olga Rosokha <helga2004@list.ru>
2020-04-05 19:48:29 +0300

    first commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,24 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) 
+    		return null;
+	const animalSound = animal.sound;
+			return animalSound;	
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+	if (marks.length === 0)
+		return 0;
+	let sumMarks = 0;
+	for (i=0; i< marks.length; i++) {
+		let mark = Number(marks[i]);
+		sumMarks += mark;
+	}
+    return Math.round(sumMarks / marks.length);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = Number(new Date());
+    const birthdayDate = Date.parse(birthday);
+    let age = (now - birthdayDate) / (( 3 * 31536000000 + 31622400000) / 4);
+    return (age >18);
 }
