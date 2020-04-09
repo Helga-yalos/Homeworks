cc1799f066221454985ae7d58d320cfb3a5470ea
Olga Rosokha <helga2004@list.ru>
2020-04-09 23:27:31 +0300

    Внесены правки + доработана задача 2

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -6,14 +6,12 @@ function parseCount(someString) {
 	if (isNaN(parseResult)) {
 		const parseError = new Error ("Невалидное значение");
 		return parseError;
-	}
+	} else 
 	return parseResult;
 }
 function validateCount(someValue) {
-	let result;
 	try { 
-		result = parseCount(someValue);
-		return result;
+		return parseCount(someValue);
 	} catch(e) {
 		 throw e;
 	}
@@ -26,39 +24,36 @@ class Triangle {
 		this.b = b;
 		this.c = c;
 		if (((this.a + this.b) < this.c) || ((this.b + this.c) < this.a) || ((this.a + this.c) < this.b)) {
-			const testError = new Error('Треугольник с такими сторонами не существует');
-			throw testError;
-      
+			throw new Error('Треугольник с такими сторонами не существует');
 		}
  }
 
 	getPerimeter() {
-		try{
 		return (this.a + this.b + this.c);
-		} catch {
-			throw new Error("Ошибка! Неправильный треугольник");
-		}
 	}
+
 	getArea() {
-		try{
 		const p = (this.a + this.b + this.c) / 2;
 		const area = Math.sqrt( p * (p - this.a) * (p - this.b) * (p - this.c));
 		return +area.toFixed(3);
-			}catch(e) {
-				throw new Error("Ошибка! Неправильный треугольник");
-			}
+	}
+}
+
+class FakeTriangle {
+	getPerimeter() {
+		throw new Error("Ошибка! Неправильный треугольник");
+	}
+	getArea() {
+		throw new Error("Ошибка! Неправильный треугольник");
 	}
 }
 
  function getTriangle(a,b,c) {
  	try { 
-     const triangle = new Triangle(a,b,c);
-        return triangle;
- 	} catch(e){ try {
- 	  return {p: triangle.getPerimeter(), s: triangle.getArea()};
-               } catch(err) {
-                       throw new Error("Ошибка! Неправильный треугольник");
-                       }
-  
-             }
+     	const triangle = new Triangle(a,b,c);
+		  return triangle;
+ 	}catch(e){ 
+ 		const triangle = new FakeTriangle();
+          return triangle;  
+    }
  }
