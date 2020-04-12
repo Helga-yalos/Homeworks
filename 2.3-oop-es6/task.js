a1c31d5d7f023ff7ca16027ab812cdded0219e49
Olga Rosokha <helga2004@list.ru>
2020-04-12 22:56:27 +0300

    Задача 1/2 доработана, 3 - решена

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -2,7 +2,7 @@
 //Task 1, 2
 
 class Weapon {
-	constructor(name,attack,durability,range) {
+	constructor({name,attack,durability,range}) {
 		this.name = name;
 		this.attack = attack;
 		this.durability = durability;
@@ -22,7 +22,7 @@ class Weapon {
 			return this.attack;
 		if ((this.durability <= 0) || (isNaN(this.durability)) || (this.durability == undefined) || (this.initialDurability == undefined) || (this.initialDurability <= 0))
 			return this.attack = 0;
-		if ((this.initialDurability) >= (0.3 * this.durability)) 
+		if ((this.durability) >= (0.3 * this.initialDurability)) 
 			return this.attack;
 			else
 				return (this.attack / 2);
@@ -33,19 +33,16 @@ class Weapon {
 	}
 };
 
-class OldSword extends Weapon {
-	constructor(name,attack,durability,range){
-		super(name,attack,durability,range);
-		this.name = 'Старый меч';
-		this.attack = 20;
-		this.durability = 10;
-		this.range = 1;
-	}
-}
+const oldSword = new Weapon({
+  name: 'Старый меч',
+  attack: 20,
+  durability: 10,
+  range: 1,
+});
 
 class Arm extends Weapon {
-	constructor (name,attack,durability,range){
-		super(name,attack,durability,range);
+	constructor(name,attack,durability,range){
+		super({name,attack,durability,range});
 		this.name = 'Рука';
 		this.attack = 1;
 		this.durability = Infinity;
@@ -55,8 +52,8 @@ class Arm extends Weapon {
 };
 
 class Bow extends Weapon {
-	constructor (name,attack,durability,range){
-		super(name,attack,durability,range);
+	constructor(name,attack,durability,range){
+		super({name,attack,durability,range});
 		this.name = 'Лук';
 		this.attack = 10;
 		this.durability = 200;
@@ -66,7 +63,7 @@ class Bow extends Weapon {
 }
 
 class LongBow extends Bow {
-	constructor (name,attack,durability,range){
+	constructor(name,attack,durability,range){
 		super(name,attack,durability,range);
 		this.name = 'Длинный лук';
 		this.attack = 15;
@@ -75,8 +72,8 @@ class LongBow extends Bow {
 }
 
 class Sword extends Weapon {
-	constructor (name,attack,durability,range){
-		super(name,attack,durability,range);
+	constructor(name,attack,durability,range){
+		super({name,attack,durability,range});
 		this.name = 'Меч';
 		this.attack = 25;
 		this.durability = 500;
@@ -86,7 +83,7 @@ class Sword extends Weapon {
 }
 
 class Axe extends Sword {
-	constructor (name,attack,durability,range){
+	constructor(name,attack,durability,range){
 		super(name,attack,durability,range);
 		this.name = 'Секира';
 		this.attack = 27;
@@ -96,8 +93,8 @@ class Axe extends Sword {
 }
 
 class Knife extends Weapon {
-	constructor (name,attack,durability,range){
-		super(name,attack,durability,range);
+	constructor(name,attack,durability,range){
+		super({name,attack,durability,range});
 		this.name = 'Нож';
 		this.attack = 5;
 		this.durability = 300;
@@ -107,8 +104,8 @@ class Knife extends Weapon {
 }
 
 class Staff extends Weapon {
-	constructor (name,attack,durability,range){
-		super(name,attack,durability,range);
+	constructor(name,attack,durability,range){
+		super({name,attack,durability,range});
 		this.name = 'Посох';
 		this.attack = 8;
 		this.durability = 300;
@@ -118,7 +115,7 @@ class Staff extends Weapon {
 } 
 
 class StormStaff extends Staff {
-	constructor (name,attack,durability,range){
+	constructor(name,attack,durability,range){
 		super(name,attack,durability,range);
 		this.name = 'Посох Бури';
 		this.attack = 10;
@@ -127,9 +124,7 @@ class StormStaff extends Staff {
 }
 
 
-const oldSword = new OldSword();
-const swordOld = new Weapon('Старый меч', 20, 10, 1);      
-    
+
 const arm = new Arm();
 const bow = new Bow();
 const sword = new Sword();
@@ -139,10 +134,72 @@ const longBow = new LongBow();
 const axe = new Axe();	
 const stormStaff = new StormStaff();	
 
-const swordOld = new Weapon('Старый меч', 20, 10, 1);
-  
+
 
 
 //Task 3
+class StudentLog {
+	constructor(name) {
+		this.name = name;
+	}
 
+	getName(){
+		return this.name;
+	}
 
+	addGrade(grade, subject){
+    if (!this.hasOwnProperty(`${subject}`)) {
+      this[subject] = [];    
+    }
+		if ((grade === 1) || (grade === 2) ||(grade === 3) || (grade === 4) || (grade === 5)) {
+      this[subject].push(grade);
+		  return this[subject].length;
+    } else {
+			return `Вы пытались поставить оценку ${grade} по предмету ${subject}. Допускаются только числа от 1 до 5.`;
+		}
+	}
+
+	getAverageBySubject(subject){
+		if (!this.hasOwnProperty(`${subject}`)) {
+			return 0;
+		}
+		let sum = 0;
+		for ( let i=0; i < this[subject].length; i++) {
+			sum += this[subject][i];
+		}
+		return sum / this[subject].length;
+	}
+
+	getTotalAverage() {
+    let sum = 0;
+    let number = 0;
+		for (let prop in this) {
+		let value = this[prop];
+      if (Array.isArray(value)){
+      console.log(value);
+      for ( let i=0; i < value.length; i++) {
+			sum += value[i];
+      number ++;
+      }
+		}
+    }
+    if (sum === 0) 
+    return 0;
+   
+    return (sum / number);
+	}
+}
+//const log = new StudentLog('Олег Никифоров');
+
+//log.addGrade(2, 'algebra');
+//log.addGrade(4, 'algebra');
+//console.log(log);
+//log.addGrade(5, 'geometry');
+//log.addGrade(4, 'geometry');
+//console.log(log);
+
+//console.log(log.getAverageBySubject('geometry')); // 4.5
+//console.log(log.getAverageBySubject('algebra')); // 3
+//console.log(log.getAverageBySubject('math')); // 0
+
+//console.log(log.getTotalAverage()); // 3,75
